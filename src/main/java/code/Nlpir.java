511387e91471f407773c5b4d00df6de4b08e4acc
wubin1989 <328454505@qq.com>
2017-01-20 20:48:29 +0800

    ...

--- src/main/java/code/Nlpir.java
+++ src/main/java/code/Nlpir.java
@@ -6,13 +6,19 @@ import java.nio.CharBuffer;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetEncoder;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import com.sun.jna.Native;
 
 public class Nlpir {
-	
-	private static CLibrary Instance = (CLibrary) Native
-			.loadLibrary("/home/wubin/Documents/workspace-sts-3.8.2.RELEASE/JnaTest_NLPIR/target/classes/libNLPIR.so", CLibrary.class);
-	
+
+	private static CLibrary Instance = (CLibrary) Native.loadLibrary(
+			"/home/wubin/Documents/workspace-sts-3.8.2.RELEASE/JnaTest_NLPIR/target/classes/libNLPIR.so",
+			CLibrary.class);
+
 	public static boolean init() {
 		String argu = "/home/wubin/Documents/workspace-sts-3.8.2.RELEASE/JnaTest_NLPIR/target/classes/argu";
 		int charset_type = 1;
@@ -25,41 +31,70 @@ public class Nlpir {
 		}
 		return true;
 	}
-	
+
 	public static void destroy() {
 		Instance.NLPIR_Exit();
 	}
-	
-	public static String processParagraph(String text){
+
+	public static String processParagraph(String text) {
 		String nativeBytes = null;
 		nativeBytes = Instance.NLPIR_ParagraphProcess(text, 1);
 		return nativeBytes;
 	}
-	
-	public static void addUserWord(String word){
+
+	public static void addUserWord(String word) {
 		Instance.NLPIR_AddUserWord(word);
 	}
-	
-	public static void delUserWord(String word){
+
+	public static void delUserWord(String word) {
 		Instance.NLPIR_DelUsrWord(word);
 	}
-	
-	public static String getKeyWords(String text){
+
+	public static String getKeyWords(String text) {
 		String nativeByte = Instance.NLPIR_GetKeyWords(text, 10, false);
 		return nativeByte;
 	}
-	
-	public static String getFileKeyWords(String filePath){
-		String nativeByte = Instance.NLPIR_GetFileKeyWords(filePath,
-				10, false);
+
+	public static String getFileKeyWords(String filePath) {
+		String nativeByte = Instance.NLPIR_GetFileKeyWords(filePath, 10, false);
 		return nativeByte;
 	}
-	
-	public static String getWordFreqStat(String text){
+
+	public static String getWordFreqStat(String text) {
 		String nativeByte = Instance.NLPIR_WordFreqStat(text);
 		return nativeByte;
 	}
-	
+
+	public static Map.Entry<String, Integer>[] getWordFreqStatForSpecificPos(String text, String pos, int minLength,
+			int top) {
+		String nativeByte = Instance.NLPIR_WordFreqStat(text);
+		if (nativeByte.length() != 0) {
+			String[] wordStats = nativeByte.split("#");
+			LinkedHashMap<String, Integer> lhm = new LinkedHashMap<String, Integer>();
+			for (String wordStat : wordStats) {
+				System.out.println(wordStat);
+				String[] stat = wordStat.split("/");
+				if (stat.length < 3) {
+					return null;
+				}
+				String word = stat[0];
+				String _pos = stat[1];
+				Integer freq = Integer.parseInt(stat[2]);
+				if (_pos.equals(pos) && word.length() >= minLength) {
+					lhm.put(word, freq);
+				}
+			}
+			@SuppressWarnings("unchecked")
+			Map.Entry<String, Integer>[] result = lhm.entrySet().stream()
+					.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).toArray(size -> new Map.Entry[size]);
+			if (top > 0) {
+				result = Arrays.copyOfRange(result, 0, top);
+			}
+			return result;
+		}
+		return null;
+	}
+
 	public static String transString(String aidString, String ori_encoding, String new_encoding) {
 		try {
 			return new String(aidString.getBytes(ori_encoding), new_encoding);
